aa66100f97ff6c6c8c38fac3278722dce77b8a09
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-01-22 06:45:55 +0530

    Provided the images path as root path

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,32 +1,32 @@
 const Partners = () => {
   return (
-    <div className="text-center py-10 bg-gray-50">
-      <h2 className="text-2xl font-semibold mb-8 text-gray-800">
+    <div className="py-10 text-center bg-gray-50">
+      <h2 className="mb-8 text-2xl font-semibold text-gray-800">
         We partner with the world’s best
       </h2>
-      <div className="flex justify-center items-center space-x-8">
+      <div className="flex items-center justify-center space-x-8">
         <img
-          src="../../public/images/Patners/Stripe.png"
+          src="/public/images/Patners/Stripe.png"
           alt="Stripe"
           className="h-8"
         />
         <img
-          src="../../public/images/Patners/square.png"
+          src="public/images/Patners/square.png"
           alt="Square"
           className="h-8"
         />
         <img
-          src="../../public/images/Patners/bigcommerce.png"
+          src="public/images/Patners/bigcommerce.png"
           alt="BigCommerce"
           className="h-8"
         />
         <img
-          src="../../public/images/Patners/shopify.png"
+          src="public/images/Patners/shopify.png"
           alt="Shopify"
           className="h-8"
         />
         <img
-          src="../../public/images/Patners/gusto.png"
+          src="public/images/Patners/gusto.png"
           alt="Gusto"
           className="h-8"
         />
